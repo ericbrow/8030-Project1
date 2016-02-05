3f02b5056cf8c519c73f21af1a9ac927f134f741
Eric Brown <ericbrow@users.noreply.github.com>
2016-02-05 10:45:18 -0600

    LineItem.java
    Line Item class for Project 1

---
+++ LineItem.java
@@ -0,0 +1,40 @@
+public class LineItem {
+  private String itemName;
+  private int quantity;
+  private price double;
+  
+  public LineItem(String newName, int newQty, double newPrice) {
+    itemName = newName;
+    quantity = newQty;
+    price = newPrice;
+  }
+  
+  public String getName() {
+    return itemName;
+  }
+  
+  public int getQuantity() {
+    return quantity;
+  }
+  
+  public double getPrice() {
+    return price;
+  }
+  
+  public double getTotalPrice() {
+    return price*quantity;
+  }
+  
+  public setQuantity(int newQty) {
+    quantity = newQty;
+  }
+  
+  public double setPrice(double newPrice) {
+    price = newPrice;
+  }
+  
+  public String toString() {
+  
+  }
+
+}
